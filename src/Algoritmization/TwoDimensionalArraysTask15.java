a55055f1ec6447d3c46765efafefc636ec9749a5
Rostislav Nichko <Rostislavfromminsk@gmail.com>
2020-03-12 20:29:27 +0300

    Сделал 15 - ую задачу (исправил название одного метода). Раздел Алгоритмизация Подразел массивы массивов.

---
+++ src/Algoritmization/TwoDimensionalArraysTask15.java
@@ -0,0 +1,65 @@
+package Algoritmization;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class TwoDimensionalArraysTask15 {
+    public static void main(String[] args)
+    {
+        int stroki = 11;
+        int stolbtsi = 11;
+
+        int arr[][] = сreateArray(stroki, stolbtsi);
+        int maxElem = 0;
+
+        for (int i = 0; i < stroki; i++)
+        {
+            for (int j = 0; j < stolbtsi; j++)
+            {
+                maxElem = findMaxElement(arr[i]);
+            }
+        }
+
+        System.out.println("Наибольший элемент матрицы = " + maxElem);
+
+        System.out.println("**************Искомая матрица******************");
+
+        int count = 0;
+
+        for (int i = 0; i < stroki; i++)
+        {
+            for (int j = 0; j < stolbtsi; j++)
+            {
+                count++;
+                if ((count % 2) != 0) arr[i][j] = maxElem;
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int findMaxElement(int[] arr)
+    {
+        int max = 0;
+        for(int i = 0; i < arr.length; i ++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        return max;
+    }
+
+    public static int[][] сreateArray(int x, int y) {
+        int a = 0;
+        int b = 41;
+
+        int arr[][] = new int[x][y];
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                arr[i][j] = a + (int) (Math.random() * b);
+            }
+        }
+        return arr;
+    }
+}
