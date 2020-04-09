83a8aeee3d8f25be644e1cf4991ba817fa12134e
Rostislav Nichko <Rostislavfromminsk@gmail.com>
2020-04-09 16:53:43 +0300

    Сделал 1 - ую задачу раздела Декомпозиция (подпрограммы).

---
+++ src/Algoritmization/DekompozitsiyaAndMethodsTask1.java
@@ -0,0 +1,36 @@
+package Algoritmization;
+
+public class DekompozitsiyaAndMethodsTask1
+{
+    public static void main(String[] args)
+    {
+        int a = 24;
+        int b = 333;
+
+        System.out.println("Наибольший общий делитель = " + getNod(a,b));
+        System.out.println("Наибольшее общее кратное  = " + getNok(a,b));
+
+    }
+
+    static int getNod(int a, int b)
+    {
+        if (a % b == 0)
+        {
+            return b;
+        }
+        if (b % a == 0)
+        {
+            return a;
+        }
+        if (a > b) {
+            return getNod(a%b,b);
+        }
+        return getNod(a,b%a);
+    }
+
+    private static int getNok(int a, int b)
+    {
+
+        return (a*b)/(getNod(a,b));
+    }
+}
