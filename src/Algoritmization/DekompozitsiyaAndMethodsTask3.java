2f486f018da47a60ab1340ac39e24a11ea36b322
Rostislav Nichko <Rostislavfromminsk@gmail.com>
2020-04-09 18:23:58 +0300

    Сделал 3 - ую задачу раздела Декомпозиция (подпрограммы).

---
+++ src/Algoritmization/DekompozitsiyaAndMethodsTask3.java
@@ -0,0 +1,22 @@
+package Algoritmization;
+
+public class DekompozitsiyaAndMethodsTask3
+{
+    public static void main(String[] args)
+    {
+
+        double a = 5 ; // длина стороны шестиугольника
+
+        System.out.println("Площадь правильного шестиугольника = " + findAreaOfTriangle(a)*6);
+
+    }
+
+    private static double findAreaOfTriangle(double a)
+    {
+        double rez;
+
+        rez = (Math.sqrt(3)/4)*Math.pow(a,2);
+
+        return rez;
+    }
+}
