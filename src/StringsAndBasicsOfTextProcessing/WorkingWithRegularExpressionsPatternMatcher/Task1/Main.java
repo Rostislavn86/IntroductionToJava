549d61c46a2baec6a75f2d6a18f5441eaa690026
Rostislav Nichko <Rostislavfromminsk@gmail.com>
2020-06-20 17:07:59 +0300

    3. String and basic text processing Раздел PatternMatches, сделал 1 - ую задачу.

--- src/StringsAndBasicsOfTextProcessing/WorkingWithRegularExpressionsPatternMatcher/Task1/Main.java
+++ src/StringsAndBasicsOfTextProcessing/WorkingWithRegularExpressionsPatternMatcher/Task1/Main.java
@@ -1,23 +1,10 @@
 package StringsAndBasicsOfTextProcessing.WorkingWithRegularExpressionsPatternMatcher.Task1;
 
-//1. Cоздать приложение, разбирающее текст (текст хранится в строке) и позволяющее выполнять с текстом три различных операции:
-// отсортировать абзацы по количеству предложений; в каждом предложении отсортировать слова по длине;
-// отсортировать лексемы в предложении по убыванию количества вхождений заданного символа, а в случае равенства – по алфавиту.
-
-//Read more: https://javarevisited.blogspot.com/2015/09/how-to-read-file-into-string-in-java-7.html#ixzz6OxQnNVns
-//https://www.javatpoint.com/java-pattern-split-method
-
-import org.w3c.dom.ls.LSOutput;
-
-import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Array;
-import java.sql.SQLOutput;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.function.DoubleToIntFunction;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,31 +13,60 @@ public class Main {
     {
         String contents = new String(Files.readAllBytes(Paths.get("D:\\Epam_Task\\Introduction to Java\\src\\StringsAndBasicsOfTextProcessing\\WorkingWithRegularExpressionsPatternMatcher\\Task1\\text.txt")));
 
-        // Сортировка Абзацев
-//        String[] newString;
-//
-//        newString = sortNumberOfSentence(createParagraphArray(contents),contents);
-//
-//        for (int i = 0; i < sortNumberOfSentence(createParagraphArray(contents),contents).length; i++)
-//        {
-//            System.out.println(newString[i]);
-//            if (i == 1) System.out.println();
-//        }
+        chooseVivod(contents);
 
-        // Сортировка слов по длинне в абзаце
-        //sortParagraphByTheLenghtOfTheWord(contents);
+    }
 
-        // Сортировка слов в предложении
-        //sortParagraphTheLenghtOfTheWordInSentence(contents);
+    public static void chooseVivod(String contents)
+    {
+        System.out.println("-----------------------------------------------------------------------------------");
+        System.out.println("Выбирите какой из параметров вы хотите вывести на консоль : ");
+        System.out.println("Для того что бы вывести отсортированные абзацы по количеству предложений введите '1' ");
+        System.out.println("Для того что бы вывести отсортированные в каждом предложении слова по длине введите '2' ");
+        System.out.println("Для того что бы вывести отсортировать лексемы в предложении по убыванию количества вхождений заданного символа, а в случае равенства – по алфавиту введите '3' ");
+        System.out.println("Для выхода введите любой символ (или комбинацию символов : )");
 
-        // Доделать https://qna.habr.com/q/297784
+        Scanner bukva = new Scanner(System.in);
+        String ch = bukva.nextLine();
 
-       //System.out.println(countOfSighn("1212","1"));
+        if (ch.equals("1")) {
+            System.out.println("Изначальный текст : ");
+            System.out.println(contents);
+            System.out.println("---------------------");
+            System.out.println("Ваш Запрос :");
+            paragraphSorting(contents);
+            chooseVivod(contents);
+        }
 
-        //Создать метод сортировки по алфавиту, с помощью монепуляий с массивом строк i+1
-        sortWordsInSentenceBySighn(contents);
+        if (ch.equals("2")) {
+            System.out.println("Изначальный текст : ");
+            System.out.println(contents);
+            System.out.println("---------------------");
+            System.out.println("Ваш Запрос :");
+            sortParagraphTheLenghtOfTheWordInSentence(contents);
+            chooseVivod(contents);
+        }
 
+        if (ch.equals("3")) {
+            System.out.println("Изначальный текст : ");
+            System.out.println(contents);
+            System.out.println("---------------------");
+            System.out.println("Ваш Запрос :");
+            sortWordsInSentenceBySighn(contents);
+            chooseVivod(contents);
+        }
+    }
 
+    private static void paragraphSorting(String contents)
+    {
+        String[] newString;
+
+        newString = sortNumberOfSentence(createParagraphArray(contents),contents);
+
+        for (int i = 0 ; i < newString.length; i++)
+        {
+            System.out.println(newString[i]);
+        }
     }
 
     private static int countOfSighn(String str, String sighn) {
@@ -66,26 +82,22 @@ public class Main {
         return count;
     }
 
-    private static String[] createNewString(String[] str,int[] place,String sighn)
-    {
-        String[] finalString = new String[str.length];
-
-        for(int i = place.length - 1; i > 0; i--)
-        {
-
-        }
-
-        return finalString;
-    }
-
-    private static String[] createAndSortWordsByAcountOfTheWord(String str)
+    private static String createAndSortWordsByAcountOfTheWord(String str)
     {
         String[] strArray = str.split(" ");
         Arrays.sort(strArray);
-        return strArray;
+
+        String strOut = "";
+
+        for(int i = 0; i < strArray.length; i++)
+        {
+            strOut += strArray[i] + " ";
+        }
+
+        return strOut;
     }
 
-    private static String[] srtWordsByASimbol(String str, String sighn)
+    private static void srtWordsByASimbol(String str, String sighn)
     {
         String[] finalString = str.split(" ");
         int[] countSighn = new int[finalString.length];
@@ -93,7 +105,6 @@ public class Main {
         for(int i = 0; i < finalString.length; i++)
         {
             countSighn[i] = countOfSighn(finalString[i],sighn);
-            System.out.println(countSighn[i]);
         }
 
         String[] finalStringWithAPlaces = new  String[finalString.length];
@@ -101,19 +112,80 @@ public class Main {
         for(int i = 0; i < finalStringWithAPlaces.length; i++)
         {
             finalStringWithAPlaces[i] = String.valueOf(countSighn[i]) + finalString[i];
-            System.out.println(finalStringWithAPlaces[i]);
         }
-
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!");
 
         Arrays.sort(finalStringWithAPlaces);
 
+        String arrayStringPovt0 = "";
+        String arrayStringPovt1 = "";
+        String arrayStringPovt2 = "";
+        String arrayStringPovt3 = "";
+        String arrayStringPovt4 = "";
+        String arrayStringPovt5 = "";
+        String arrayStringPovt6 = "";
+
         for(int i = 0; i < finalStringWithAPlaces.length; i++)
         {
-            System.out.println(finalStringWithAPlaces[i]);
+            Pattern p = Pattern.compile("^[0-9]");
+            Matcher m = p.matcher(finalStringWithAPlaces[i]);
+
+            while (m.find()) {
+                if (m.group().equals("0")) arrayStringPovt0 += finalStringWithAPlaces[i];
+                if (m.group().equals("1")) arrayStringPovt1 += finalStringWithAPlaces[i];
+                if (m.group().equals("2")) arrayStringPovt2 += finalStringWithAPlaces[i];
+                if (m.group().equals("3")) arrayStringPovt3 += finalStringWithAPlaces[i];
+                if (m.group().equals("4")) arrayStringPovt4 += finalStringWithAPlaces[i];
+                if (m.group().equals("5")) arrayStringPovt5 += finalStringWithAPlaces[i];
+                if (m.group().equals("6")) arrayStringPovt6 += finalStringWithAPlaces[i];
+            }
         }
 
-        return finalString;
+        arrayStringPovt0 = arrayStringPovt0.replaceAll("[0-9]"," ");
+        arrayStringPovt0 = arrayStringPovt0.trim();
+
+        arrayStringPovt1 = arrayStringPovt1.replaceAll("[0-9]"," ");
+        arrayStringPovt1 = arrayStringPovt1.trim();
+
+        arrayStringPovt2 = arrayStringPovt2.replaceAll("[0-9]"," ");
+        arrayStringPovt2 = arrayStringPovt2.trim();
+
+        arrayStringPovt3 = arrayStringPovt3.replaceAll("[0-9]"," ");
+        arrayStringPovt3 = arrayStringPovt3.trim();
+
+        arrayStringPovt4 = arrayStringPovt4.replaceAll("[0-9]"," ");
+        arrayStringPovt4 = arrayStringPovt4.trim();
+
+        arrayStringPovt5 = arrayStringPovt5.replaceAll("[0-9]"," ");
+        arrayStringPovt5 = arrayStringPovt5.trim();
+
+        arrayStringPovt6 = arrayStringPovt6.replaceAll("[0-9]"," ");
+        arrayStringPovt6 = arrayStringPovt6.trim();
+
+        createAndSortWordsByAcountOfTheWord(arrayStringPovt0);
+        createAndSortWordsByAcountOfTheWord(arrayStringPovt1);
+        createAndSortWordsByAcountOfTheWord(arrayStringPovt2);
+        createAndSortWordsByAcountOfTheWord(arrayStringPovt3);
+        createAndSortWordsByAcountOfTheWord(arrayStringPovt4);
+        createAndSortWordsByAcountOfTheWord(arrayStringPovt5);
+        createAndSortWordsByAcountOfTheWord(arrayStringPovt6);
+
+        String strForFinalArray = createAndSortWordsByAcountOfTheWord(arrayStringPovt6)
+        + createAndSortWordsByAcountOfTheWord(arrayStringPovt5)
+        + createAndSortWordsByAcountOfTheWord(arrayStringPovt4)
+        + createAndSortWordsByAcountOfTheWord(arrayStringPovt3)
+        + createAndSortWordsByAcountOfTheWord(arrayStringPovt2)
+        + createAndSortWordsByAcountOfTheWord(arrayStringPovt1)
+        + createAndSortWordsByAcountOfTheWord(arrayStringPovt0);
+
+        strForFinalArray = strForFinalArray.trim();
+
+        String[] outPutArray = strForFinalArray.split(" ");
+
+        for(int i = 0; i < outPutArray.length; i++)
+        {
+            if (outPutArray[i].equals("")) continue;
+            System.out.print(outPutArray[i] + " ");
+        }
     }
 
     private static void sortWordsInSentenceBySighn(String text)
@@ -122,44 +194,20 @@ public class Main {
 
         string = string.replaceAll("[1234567890,—-]", "");
 
-        //System.out.println(string);
-
-        int countofSentance = countOfSighn(string,"([^.!?]+[.!?])");
-
-        System.out.println(countofSentance);
-
-        String[] stringsArray = new String[countofSentance];
+        String[] stringsArray ;
 
         stringsArray = string.split("[.!?]");
 
-//        for(int i = 0; i <stringsArray.length; i++)
-//        {
-//            System.out.println(i + " : " +  stringsArray[i]);
-//        }
+        System.out.println("Введите символ вхождение которого нужно посчитать в строке : ");
 
+        Scanner bukva = new Scanner(System.in);
+        String ch = bukva.nextLine();
 
-
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!");
-
-       // Arrays.sort(stringsArray,new StringLengthComparator());
-
-        srtWordsByASimbol(stringsArray[0],"а");
-
-//        System.out.println("Сортировка по длинне  предложения : ");
-//        for(int i = 0; i <stringsArray.length; i++)
-//        {
-//            System.out.println(stringsArray[i]);
-//           // System.out.println(i + " : " +  stringsArray[i]);
-//        }
-
-
-
-
-
-
-
-
-
+        for(int i = 0; i <stringsArray.length; i++)
+        {
+            srtWordsByASimbol(stringsArray[i],ch);
+            System.out.println();
+        }
     }
 
 
@@ -189,66 +237,11 @@ public class Main {
             textFinal += Arrays.toString(stringArray2);
         }
 
-
         //https://ru.stackoverflow.com/questions/103640/%D0%A3%D0%B4%D0%B0%D0%BB%D0%B8%D1%82%D1%8C-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB%D1%8B-%D0%B2-java
 
         textFinal = textFinal.replaceAll("[,.!?:;]", "");
 
         System.out.println(textFinal);
-
-    }
-
-    private static void sortParagraphByTheLenghtOfTheWord(String text)
-    {
-        String[] arrayOfSentence = new String[createParagraphArray(text).length - 1];
-
-          //      String[] arrayOfSentence = createParagraphArray(text);
-
-        System.out.println("-------------------------------");
-
-        for(int i = 0; i < arrayOfSentence.length; i++)
-        {
-            //System.out.println(createParagraphArray(text)[0]);
-            if (createParagraphArray(text)[i].equals(createParagraphArray(text)[0])) continue;
-            arrayOfSentence[i]= createParagraphArray(text)[i];
-           // System.out.println("1" + arrayOfSentence[i]);
-        }
-
-        String wordsOfArrayOfSentence = "";
-
-        for(int i = 0; i < arrayOfSentence.length; i++)
-        {
-
-             }
-
-        Pattern p1 = Pattern.compile("[а-юА-Юa-zA-Z\\s]");
-        Matcher m1 = p1.matcher(arrayOfSentence[2]);
-
-        for(int i = 0; i < 300; i++)
-        {
-            if (m1.find())
-            {
-                wordsOfArrayOfSentence += m1.group();
-            }
-        }
-
-        String[] wordsOfArrayOfSentenceArray = wordsOfArrayOfSentence.split(" ");
-
-        for(int i = 0; i < wordsOfArrayOfSentenceArray.length; i++)
-        {
-            System.out.println(wordsOfArrayOfSentenceArray[i]);
-        }
-
-        System.out.println(wordsOfArrayOfSentence);
-
-        System.out.println("---------------------------------");
-
-        //https://stackoverflow.com/questions/35866240/how-to-sort-string-array-by-length-using-arrays-sort
-
-        Arrays.sort(wordsOfArrayOfSentenceArray,new StringLengthComparator());
-
-        System.out.println(Arrays.toString(wordsOfArrayOfSentenceArray));
-
     }
 
     private static String[] sortNumberOfSentence(String[] str, String text)
@@ -271,9 +264,7 @@ public class Main {
                 if (numbreOfSentence[i] == countOfSentence(createParagraphArray(text)[j])) sotredString[i] = str[j];
             }
         }
-
         return sotredString;
-
     }
 
     private static int countOfSentence(String str) {
@@ -297,7 +288,6 @@ public class Main {
 
         return strPar;
     }
-
 }
 
 
