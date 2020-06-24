2cd38c1e9b73bcdb9304e5f0a7de1fcc35749f7a
Rostislav Nichko <Rostislavfromminsk@gmail.com>
2020-06-24 19:23:59 +0300

    3. String and basic text processing Раздел PatternMatches, доделать 2 - ую задачу.

--- src/StringsAndBasicsOfTextProcessing/WorkingWithRegularExpressionsPatternMatcher/Task2/Main.java
+++ src/StringsAndBasicsOfTextProcessing/WorkingWithRegularExpressionsPatternMatcher/Task2/Main.java
@@ -3,7 +3,6 @@ package StringsAndBasicsOfTextProcessing.WorkingWithRegularExpressionsPatternMat
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,62 +10,98 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String contents = new String(Files.readAllBytes(Paths.get("D:\\Epam_Task\\Introduction to Java\\src\\StringsAndBasicsOfTextProcessing\\WorkingWithRegularExpressionsPatternMatcher\\Task2\\text.txt")));
 
-        Pattern p1 = Pattern.compile("<\\b.+?>");//открывающий тег
-        Matcher m1 = p1.matcher(contents);
+        showOpenTegFromText(contents);
 
-        System.out.println("Открывающий тэг : ");
+        System.out.println("------------------------------------");
 
-        while (m1.find()) {
-            System.out.println(m1.group() + " ");
+        showCloseTegFromText(contents);
+
+        System.out.println("------------------------------------");
+
+        showContentOfTheTegs(contents);
+
+        System.out.println("------------------------------------");
+
+        showTegWithoutBody(contents);
+    }
+
+    private static void showTegWithoutBody(String text)
+    {
+        Pattern p4 = Pattern.compile(".+[/>]+?>");//закрывающий тег
+
+        Matcher m4 = p4.matcher(text);
+
+        System.out.println("Тэг без тела : ");
+
+        while (m4.find())
+        {
+            System.out.println(m4.group() + " ");
         }
+    }
 
+    private static void showCloseTegFromText(String text)
+    {
         Pattern p2 = Pattern.compile("<[/].+?>");//закрывающий тег
-        Matcher m2 = p2.matcher(contents);
+        Matcher m2 = p2.matcher(text);
 
         System.out.println("Закрывающий тэг :");
 
         while (m2.find()) {
             System.out.println(m2.group() + " ");
         }
+    }
 
-        Pattern p3 = Pattern.compile("<\\b.+?>(.+)<[/].+?>");//Содержание тэга
-        Matcher m3 = p3.matcher(contents);
+    private static void showOpenTegFromText(String text)
+    {
+        Pattern p1 = Pattern.compile("<\\b.+?>");//открывающий тег
+        Matcher m1 = p1.matcher(text);
+
+        System.out.println("Открывающий тэг : ");
+
+        while (m1.find()) {
+            System.out.println(m1.group() + " ");
+        }
+    }
+
+    private static void showContentOfTheTegs(String text)
+    {
+        Pattern p3 = Pattern.compile("(<\\b.+?>)(.+)<[/](.+?>)");//Содержание тэга
+        Matcher m3 = p3.matcher(text);
 
         System.out.println("Содержание тэга :");
 
         while (m3.find())
         {
-            System.out.println(m3.group() + " ");
+            System.out.println(m3.group(2) + " ");
         }
 
-//        Pattern p4 = Pattern.compile("<notes>(\\S+)</notes>");//Открывающий тег
-//        Matcher m4 = p4.matcher(contents);
+        System.out.println("<note id = \"1\">");
 
-        //https://issue.life/questions/6560672/
+        System.out.println("************************************");
 
-        System.out.println("Notes тег :");
+        System.out.println("тэг <notes> (содержание)");
 
         final Pattern pattern = Pattern.compile("<notes>(.+?)</notes>", Pattern.DOTALL);
-        final Matcher matcher = pattern.matcher(contents);
+        final Matcher matcher = pattern.matcher(text);
         matcher.find();
-        System.out.println(matcher.group(1)); // Prints String I want to extract
+        System.out.println(matcher.group());
 
-        System.out.println("note 1");
+        System.out.println("************************************");
+
+        System.out.println("тег <note id = 1 сождержание");
 
         final Pattern pattern2 = Pattern.compile("<note id = \"1\">(.+?)</note>", Pattern.DOTALL);
-        final Matcher matcher2 = pattern2.matcher(contents);
+        final Matcher matcher2 = pattern2.matcher(text);
         matcher2.find();
-        System.out.println(matcher2.group(1)); // Prints String I want to extract
+        System.out.println(matcher2.group());
 
-        System.out.println("note 2");
+        System.out.println("************************************");
+
+        System.out.println("тег <note id = 2 сождержание");
 
         final Pattern pattern3 = Pattern.compile("<note id = \"2\">(.+?)</note>", Pattern.DOTALL);
-        final Matcher matcher3 = pattern3.matcher(contents);
+        final Matcher matcher3 = pattern3.matcher(text);
         matcher3.find();
-        System.out.println(matcher3.group(1)); // Prints String I want to extract
-
-
+        System.out.println(matcher3.group());
     }
-
-
 }
